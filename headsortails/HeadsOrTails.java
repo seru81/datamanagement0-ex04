cf49e787733f96f99c65146825f5014c8a67bbf6
Kuribayashi <s1290154@u-aizu.ac.jp>
2022-07-08 11:42:58 +0900

    HoT: initial revision

---
+++ headsortails/HeadsOrTails.java
@@ -0,0 +1,33 @@
+class Coin{
+    private static int heads;
+    private static int tails;
+
+    public static int getHeads () {
+        return heads;
+    }
+
+    public static int getTails () {
+        return tails;
+    }
+
+    public static String toss () {
+        int side = ((int)(Math.random()*10))%2;
+        if (side == 0) {
+            heads++;
+            return "Heads";
+        } else {
+            tails++;
+            return "Tails";
+        }
+    }
+}
+
+public class HeadsOrTails{
+    public static void main (String[] arg) {
+        System.out.println("Tossing a coin...");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Round " + i + ": " + Coin.toss());
+        }
+        System.out.println("Heads: " + Coin.getHeads() + ", Tails: " + Coin.getTails());
+    }
+}
